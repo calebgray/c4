22626f7b93f6a45a2416611ecc6a23c06ba15f60
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2014-12-19 13:01:16 +0700

    add x86 jit (c5 -> c6)
    This is code rip from https://github.com/EarlGray/c4.git, commit
    947b27678fd34c295745fb6771c44770dbce0946 with some debugging code
    removed.

--- c4.c
+++ c4.c
@@ -9,6 +9,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <memory.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #ifdef __x86_64__
 #define int long
 #endif
@@ -16,7 +21,7 @@
 char *p, *lp, // current position in source code
      *data;   // data/bss pointer
 
-int *e, *le,  // current position in emitted code
+int *e, *le, *text,  // current position in emitted code
     *id,      // currently parsed identifier
     *sym,     // symbol table (simple list of identifiers)
     tk,       // current token
@@ -401,21 +406,116 @@ int run(int poolsz, int *start, int argc, char **argv)
   }
 }
 
+int jit(int poolsz, int *start, int argc, char **argv)
+{
+  int *pc;
+  int i, tmp;        // temps
+  char *je;          // current position in emitted native code
+  char *jitmem;      // executable memory for JIT-compiled native code
+
+  // setup jit memory
+  jitmem = mmap(0, poolsz, PROT_EXEC | PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
+  if (!jitmem) { printf("could not mmap(%d) jit executable memory\n", poolsz); return -1; }
+
+  // first pass: emit native code
+  pc = text + 1; je = jitmem; line = 0;
+  while (pc <= e) {
+    i = *pc;
+    *pc++ = ((int)je << 8) | i; // for later relocation of JMP/JSR/BZ/BNZ
+    if (i == LEA) {
+      i = 4 * *pc++; if (i < -128 || i > 127) { printf("jit: LEA out of bounds\n"); return -1; }
+      *(int*)je = 0x458d; je = je + 2; *je++ = i;  // leal $(4 * n)(%ebp), %eax
+    }
+    else if (i == ENT) {
+      i = 4 * *pc++; if (i < -128 || i > 127) { printf("jit: ENT out of bounds\n"); return -1; }
+      *(int *)je = 0xe58955; je = je + 3;  // push %ebp; movl %esp, %ebp
+      if (i > 0) { *(int *)je = 0xec83; je = je + 2; *(int*)je++ = i; } // subl $(i*4), %esp
+    }
+    else if (i == IMM) { *je++ = 0xb8; *(int *)je = *pc++; je = je + 4; } // movl $imm, %eax
+    else if (i == ADJ) { i = 4 * *pc++; *(int *)je = 0xc483; je = je + 2; *(int *)je = i; je++; } // addl $(n * 4), %esp
+    else if (i == PSH)   *(int *)je++ = 0x50;                    // push %eax
+    else if (i == LEV) { *(int *)je = 0xc35dec89; je = je + 4; } // mov %ebp, %esp; pop %ebp; ret
+    else if (i == LI)  { *(int *)je = 0x008b;     je = je + 2; } // movl (%eax), %eax
+    else if (i == LC)  { *(int *)je = 0x00b60f;   je = je + 3; } // movzbl (%eax), %eax
+    else if (i == SI)  { *(int *)je = 0x018959;   je = je + 3; } // pop %ecx; movl %eax, (%ecx)
+    else if (i == SC)  { *(int *)je = 0x018859;   je = je + 3; } // pop %ecx; movb %al, (%ecx)
+    else if (i == OR)  { *(int *)je = 0xc80959;   je = je + 3; } // pop %ecx; orl %ecx, %eax
+    else if (i == XOR) { *(int *)je = 0xc83159;   je = je + 3; } // pop %ecx; xorl %ecx, %eax
+    else if (i == AND) { *(int *)je = 0xc82159;   je = je + 3; } // pop %ecx; andl %ecx, %eax
+    else if (EQ <= i && i <= GE) {
+        *(int*)je=0x0fc13959; je+=4; *(int*)je=0x9866c094; // pop %ecx; cmp %ecx, %eax; sete %al; cbw; - EQ
+        if      (i == NE)  { *je = 0x95; } // setne %al
+        else if (i == LT)  { *je = 0x9c; } // setl %al
+        else if (i == GT)  { *je = 0x9f; } // setg %al
+        else if (i == LE)  { *je = 0x9e; } // setle %al
+        else if (i == GE)  { *je = 0x9d; } // setge %al
+        je+=4; *je++=0x98;  // cwde
+    }
+    else if (i == SHL) { *(int*)je = 0xe0d39159; je = je + 4; } // pop %ecx; xchg %eax, %ecx; shl %cl, %eax
+    else if (i == SHR) { *(int*)je = 0xe8d39159; je = je + 4; } // pop %ecx; xchg %eax, %ecx; shr %cl, %eax
+    else if (i == ADD) { *(int*)je = 0xc80159;   je = je + 3; } // pop %ecx; addl %ecx, %eax
+    else if (i == SUB) { *(int*)je = 0xc8299159; je = je + 4; } // pop %ecx; xchg %eax, %ecx; subl %ecx, %eax
+    else if (i == MUL) { *(int*)je = 0xc1af0f59; je = je + 4; } // pop %ecx; imul %ecx, %eax
+    else if (i == DIV) { *(int*)je = 0xf9f79159; je = je + 4; } // pop %ecx; xchg %eax, %ecx; idiv %ecx, %eax
+    else if (i == MOD) { *(int*)je = 0xd2319159; je += 4; *(int *)je = 0x92f9f7; je += 3; }
+    else if (i == JMP) { ++pc; *je       = 0xe9;     je = je + 5; } // jmp <off32>
+    else if (i == JSR) { ++pc; *je       = 0xe8;     je = je + 5; } // call <off32>
+    else if (i == BZ)  { ++pc; *(int*)je = 0x840fc085; je = je + 8; } // test %eax, %eax; jz <off32>
+    else if (i == BNZ) { ++pc; *(int*)je = 0x850fc085; je = je + 8; } // test %eax, %eax; jnz <off32>
+    else if (i >= OPEN) {
+      if      (i == OPEN) tmp = (int)open;   else if (i == READ) tmp = (int)read;
+      else if (i == CLOS) tmp = (int)close;  else if (i == PRTF) tmp = (int)printf;
+      else if (i == MALC) tmp = (int)malloc; else if (i == MSET) tmp = (int)memset;
+      else if (i == MCMP) tmp = (int)memcmp; else if (i == EXIT) tmp = (int)exit;
+      if (*pc++ == ADJ) { i = *pc++; } else { printf("no ADJ after native proc!\n"); exit(2); }
+      *je++ = 0xb9; *(int*)je = i << 2; je += 4;  // movl $(4 * n), %ecx;
+      *(int*)je = 0xce29e689; je += 4; // mov %esp, %esi; sub %ecx, %esi;  -- %esi will adjust the stack
+      *(int*)je = 0x8302e9c1; je += 4; // shr $2, %ecx; and                -- alignment of %esp for OS X
+      *(int*)je = 0x895af0e6; je += 4; // $0xfffffff0, %esi; pop %edx; mov..
+      *(int*)je = 0xe2fc8e54; je += 4; // ..%edx, -4(%esi,%ecx,4); loop..  -- reversing args order
+      *(int*)je = 0xe8f487f9; je += 4; // ..<'pop' offset>; xchg %esi, %esp; call    -- saving old stack in %esi
+      *(int*)je = tmp - (int)(je + 4); je = je + 4; // <*tmp offset>;
+      *(int*)je = 0xf487; je += 2;     // xchg %esi, %esp  -- ADJ, back to old stack without arguments
+    }
+    else { printf("code generation failed for %d!\n", i); return -1; }
+  }
+
+  // second pass, relocation
+  pc = text + 1;
+  while (pc <= e) {
+    i = *pc & 0xff;
+    je = (char*)(((unsigned)*pc++ >> 8) | ((unsigned)jitmem & 0xff000000)); // MSB is restored from jitmem
+    if (i == JSR || i == JMP || i == BZ || i == BNZ) {
+        tmp = (*(unsigned*)(*pc++) >> 8) | ((unsigned)jitmem & 0xff000000); // extract address
+        if      (i == JSR || i == JMP) { je += 1; *(int*)je = tmp - (int)(je + 4); }
+        else if (i == BZ  || i == BNZ) { je += 4; *(int*)je = tmp - (int)(je + 4); }
+    }
+    else if (i < LEV) { ++pc; }
+  }
+
+  // run jitted code
+  int (*jitmain)(char**, int); // c4 vm pushes first argument first, unlike cdecl
+  jitmain = (void *)(*(unsigned*)start >> 8 | ((unsigned)jitmem & 0xff000000));
+  return jitmain(argv, argc);
+}
+
 int main(int argc, char **argv)
 {
   int fd, bt, ty, poolsz, *idmain;
-  int i; // temps
+  int i, usejit; // temps
 
+  usejit = 0;
   --argc; ++argv;
   if (argc > 0 && **argv == '-' && (*argv)[1] == 's') { src = 1; --argc; ++argv; }
   if (argc > 0 && **argv == '-' && (*argv)[1] == 'd') { debug = 1; --argc; ++argv; }
+  if (argc > 0 && **argv == '-' && (*argv)[1] == 'j') { usejit = 1; --argc; ++argv; }
   if (argc < 1) { printf("usage: c4 [-s] [-d] file ...\n"); return -1; }
 
   if ((fd = open(*argv, 0)) < 0) { printf("could not open(%s)\n", *argv); return -1; }
 
   poolsz = 256*1024; // arbitrary size
   if (!(sym = malloc(poolsz))) { printf("could not malloc(%d) symbol area\n", poolsz); return -1; }
-  if (!(le = e = malloc(poolsz))) { printf("could not malloc(%d) text area\n", poolsz); return -1; }
+  if (!(text = le = e = malloc(poolsz))) { printf("could not malloc(%d) text area\n", poolsz); return -1; }
   if (!(data = malloc(poolsz))) { printf("could not malloc(%d) data area\n", poolsz); return -1; }
 
   memset(sym,  0, poolsz);
@@ -528,5 +628,7 @@ int main(int argc, char **argv)
     }
     next();
   }
+  if (usejit)
+    return jit(poolsz, (int *)idmain[Val], argc, argv);
   return run(poolsz, (int *)idmain[Val], argc, argv);
 }
